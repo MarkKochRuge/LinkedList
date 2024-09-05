e63ef57a966be483b5fb468474092761c0081d24
MarkKochRuge <165993382+MarkKochRuge@users.noreply.github.com>
2024-09-05 12:12:34 +0200

    gggg

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,6 +1,7 @@
 ﻿//Author: Per Larsen
 
 using System;
+using System.Collections;
 using System.Reflection.Metadata.Ecma335;
 
 public class LLdemo
@@ -16,15 +17,47 @@ public class LLdemo
         s.PrintUlige();
 
         int sum = s.Beregnsum();
-        Console.WriteLine(sum);
+        Console.WriteLine("\nBeregner summen: " + sum);
         Console.WriteLine();
 
+        Console.WriteLine("indsæt efter 2");
         s.IndSetEfter(2, 200);
         s.printAll();
         Console.WriteLine();
 
+        Console.WriteLine("Slet element 2");
         s.SletElement(2) ;
         s.printAll();
+        Console.WriteLine();
+
+        Console.WriteLine("Tilføj under top");
+        s.AddUnderTop(220);
+        s.printAll();
+        Console.WriteLine();
+
+        int hent = s.HentFørste();
+        Console.WriteLine("Henter den første i listen:" + hent );
+        Console.WriteLine();
+
+        Console.WriteLine("Slet første");
+        s.SletFørste();
+        s.printAll();
+        Console.WriteLine();
+
+        s.IndSetEfter(0, 6);
+        int hent2 = s.HentOgSletFoerste();
+        Console.WriteLine("hentet og slettet første værdi: " + hent2);
+        Console.WriteLine();
+        s.printAll();
+        Console.WriteLine();
+
+        double snit = s.BeregnSnit();
+        Console.WriteLine("Beregnet snit: " + snit);
+        Console.WriteLine();
+
+        LinkedList linkedList = s.Getinterval(4, 9);
+        linkedList.printAll();
+
     }
 }
 
@@ -72,7 +105,6 @@ class LinkedList
 
     public int Beregnsum()
     {
-        Console.WriteLine("\nBeregner summen af alt data:");
         Node temp = Head;
         int sum = 0;
         while (temp != null)
@@ -116,35 +148,109 @@ class LinkedList
         }
     }
 
+
+    public void AddUnderTop(int tal) 
+    {
+        Node nyNode = new Node();
+        nyNode.data = tal;
+        Node temp = Head;
+        int count = 0;
+
+        while (temp != null && count < 1 - 1)
+        {
+            temp = temp.next;
+            count++;
+        }
+
+        nyNode.next = temp.next;
+        temp.next = nyNode;
+        
+    }
+
     public void SletElement(int pos)
     {
         Node temp = Head;
-        // If the position is 0, delete the head node
         if (pos == 0)
         {
-            Head = temp.next; // Change head to the next node
+            Head = temp.next;
         }
 
-        // Find the previous node of the node to be deleted
         for (int i = 0; temp != null && i < pos - 1; i++)
         {
             temp = temp.next;
         }
 
-        // If the position is greater than the number of nodes
         if (temp == null || temp.next == null)
         {
             Console.WriteLine($"Position {pos} er ude af rækkevidde.");
         }
 
-        // Node temp.next is the node to be deleted
         Node next = temp.next.next;
+        temp.next = next;
+    }
 
-        // Unlink the node from the linked list
+    public void SletFørste()
+    {
+        Node temp = Head;
+        Head = temp.next;
+        Node next = temp.next.next;
         temp.next = next;
 
+    }
 
 
+
+    public int HentFørste()
+    {
+        int hent = 0;
+        Node temp = Head;
+        hent = temp.data;
+        return hent;
+    }
+
+    public int HentOgSletFoerste()
+    {
+        int hent = 0;
+        Node temp = Head;
+        hent = temp.data;
+
+        Head = temp.next;
+        Node next = temp.next.next;
+        temp.next = next;
+        return hent;
+    }
+
+    public double BeregnSnit()
+    {
+        Node temp = Head;
+        int count = 0;
+        int sum = Beregnsum();
+        while (temp != null)
+        {
+            temp = temp.next;
+            count++;
+        }
+        double snit = sum / count;
+        return snit;
+    }
+
+    public LinkedList Getinterval(int i1, int i2)
+    {
+        LinkedList l = new LinkedList();
+        Node temp = Head;
+        while (temp != null)
+        {
+            temp = temp.next;
+            if (temp != null || temp.data < i2 || temp.data > i1)
+            {
+                l.add(temp.data);
+            }
+            else
+            {
+                return l;
+            }
+        }
+        return l;
     }
 }
 
